dbd4d314418ca0541014df2a01f7aef00a0ccf22
RobF <raam337@gmail.com>
2024-01-22 22:07:20 +0000

    JS script WIP

--- starter/script.js
+++ starter/script.js
@@ -1,12 +1,25 @@
 
 var table_body = $("#table-main");
-var template = table_body.children().eq(0).clone();
-//table_body.children().eq(0).addClass("d-none");
-
-function renderTable(){
-    
-    console.log(template);
-    table_body.append(template);
-}
+var template = table_body.children().eq(0);
+var today = $("#currentDay");
 
 renderTable();
+updateDay();
+table_body.children().eq(0).addClass("d-none");
+
+
+function renderTable(){
+    var local_tasks = JSON.parse(localStorage.getItem("schedulerTasks"));
+    console.log(local_tasks);
+
+    for (let i = 0; i < 24; i++) {
+        var clonedTemp = template.clone();
+        clonedTemp.find(".hour").text(dayjs().set("hour", i).format("H a"));
+        table_body.append(clonedTemp);
+    }
+    
+}
+
+function updateDay(){
+    today.text(dayjs().format());
+}
